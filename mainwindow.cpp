430f0330af3665a3c02310151ec27e3863a6b877
PRATEEK GUPTA <70360396+Prateekphi@users.noreply.github.com>
2022-08-18 07:14:25 +0530

    Styling changes

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -10,6 +10,8 @@
 
 using namespace std;
 string fname = "";
+int maxX=0, maxY=0;
+int minX=0, minY=0; // At least zero to be seen on graph negative can also be seen
 
 
 MainWindow::MainWindow(QWidget *parent)
@@ -17,6 +19,7 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    this->setStyleSheet("background-color: #a6a6a6;");
 }
 
 MainWindow::~MainWindow()
@@ -29,20 +32,79 @@ void MainWindow::on_pushButton_clicked()
     QString textEditText = ui->plainTextEdit->toPlainText();
     fname = textEditText.toStdString();
     QLineSeries* series = makeSeries();
-    /*
-    //createSeries(*series);
-    if(!chart->isEmpty())
-        chart->removeAllSeries();// crash here
 
-    //chartView->close();
-    */
     QChart *chart = new QChart();
     chart->addSeries(series);
     chart->createDefaultAxes();
+    // Customize series
+    QPen pen(QRgb(0xfdb157));
+    pen.setWidth(4);
+    series->setPen(pen);
+    // Customize chart title
+    QFont font;
+    font.setPixelSize(18);
+    font.setBold(true);
+    chart->setTitleFont(font);
+    chart->setTitleBrush(QBrush(QRgb(0x008000)));
     chart->setTitle("Created By Prateek Gupta");
+    // BackGround Styling
+    QLinearGradient backgroundGradient;
+    backgroundGradient.setStart(QPointF(0, 0));
+    backgroundGradient.setFinalStop(QPointF(0, 10));
+    backgroundGradient.setColorAt(0.0, QRgb(0xd2d0d1));
+    backgroundGradient.setColorAt(1.0, QRgb(0x4c4547));
+    backgroundGradient.setCoordinateMode(QGradient::ObjectBoundingMode);
+    chart->setBackgroundBrush(backgroundGradient);
+    // Customize plot area background
+    QLinearGradient plotAreaGradient;
+    plotAreaGradient.setStart(QPointF(0, 1));
+    plotAreaGradient.setFinalStop(QPointF(1, 0));
+    plotAreaGradient.setColorAt(0.0, QRgb(0x555555));
+    plotAreaGradient.setColorAt(1.0, QRgb(0x55aa55));
+    plotAreaGradient.setCoordinateMode(QGradient::ObjectBoundingMode);
+    chart->setPlotAreaBackgroundBrush(plotAreaGradient);
+    chart->setPlotAreaBackgroundVisible(true);
 
-    chart->legend()->setVisible(true);
-    chart->legend()->setAlignment(Qt::AlignBottom);
+    QCategoryAxis *axisX = new QCategoryAxis();
+    QCategoryAxis *axisY = new QCategoryAxis();
+
+    // Customize axis label font
+    QFont labelsFont;
+    labelsFont.setPixelSize(16);
+    axisX->setLabelsFont(labelsFont);
+    axisY->setLabelsFont(labelsFont);
+
+    // Customize axis colors
+    QPen axisPen(QRgb(0xd18952));
+    axisPen.setWidth(2);
+    axisX->setLinePen(axisPen);
+    axisY->setLinePen(axisPen);
+
+    // Customize axis label colors
+    QBrush axisBrush(Qt::white);
+    axisX->setLabelsBrush(axisBrush);
+    axisY->setLabelsBrush(axisBrush);
+
+    // Customize grid lines and shades
+    axisX->setGridLineVisible(false);
+    axisY->setGridLineVisible(false);
+    axisY->setShadesPen(Qt::NoPen);
+    axisY->setShadesBrush(QBrush(QColor(0x99, 0xcc, 0xcc, 0x55)));
+    axisY->setShadesVisible(true);
+
+    axisX->append("X-coordinates",maxX);
+    axisY->append("Y-coordinates",maxY);
+    axisX->setRange(minX, maxX+1);
+    axisY->setRange(minY, maxY+1);
+        // You can have (minY-1) also for better visiblity of chart
+        // Pending : AxisY set oreientation to vertical
+    chart->addAxis(axisX, Qt::AlignBottom);
+    chart->addAxis(axisY, Qt::AlignLeft);
+    series->attachAxis(axisX);
+    series->attachAxis(axisY);
+
+    chart->legend()->setVisible(false);
+    //chart->legend()->setAlignment(Qt::AlignBottom);
     QChartView* chartView = new QChartView(chart);
 
     chartView->setRenderHint(QPainter::Antialiasing);
@@ -73,7 +135,14 @@ QLineSeries* MainWindow:: makeSeries()
             content.push_back(row);
         }
         for(int i=0;i<content.size();i++)
+        {
             series->append(stoi(content[i][0]),stoi(content[i][1]));
+            maxX = max(maxX, stoi(content[i][0]));
+            minX = min(minX, stoi(content[i][0]));
+            maxY = max(maxY, stoi(content[i][1]));
+            minY = min(minY, stoi(content[i][1]));
+        }
+
     }
     else
         cout<<"Could not open the file\n";
